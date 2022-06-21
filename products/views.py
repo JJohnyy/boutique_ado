ff68aacdf184198cf48e91176e37310e7b05e989
JJohnyy <jan.jasa@email.cz>
2022-06-21 15:07:48 +0000

    views.py

--- products/views.py
+++ products/views.py
@@ -10,15 +10,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category_name_in=categories)
-            categories = Category.objects.filter(name_in=categories)
-            
-    if request.GET:
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
